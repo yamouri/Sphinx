67b714ed1c237102f9c903667310cb70aea1d1bb
eleve <eleve@machineSNIR>
2017-02-22 09:34:59 +0100

    From Netbean To GitHub

---
+++ ProjetSphinx/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 22 février 2017, 09:33
+ */
+
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
