379adc2d127be41ef481f2d5abd4046278c0a79b
eleve <eleve@machineSNIR>
2017-02-22 15:38:43 +0100

    From Netbean To GitHub

---
+++ SphinxProjet/main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc , char *argv[])
+{
+    int socket_desc;
+    struct sockaddr_in server;
+    char *message;
+     
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+         
+    server.sin_addr.s_addr = inet_addr("74.125.235.20");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 80 );
+ 
+    //Connect to remote server
+    if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        puts("connect error");
+        return 1;
+    }
+     
+    puts("Connected\n");
+     
+    //Send some data
+    message = "GET / HTTP/1.1\r\n\r\n";
+    if( send(socket_desc , message , strlen(message) , 0) < 0)
+    {
+        puts("Send failed");
+        return 1;
+    }
+    puts("Data Send\n");
+     
+    return 0;
+}
